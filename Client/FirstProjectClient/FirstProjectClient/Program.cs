6030ae985d39fdaed6beae5ce9e9da8588410e84
Chris-Han3en <75200147+Chris-Han3en@users.noreply.github.com>
2021-08-12 13:55:23 +0100

    Add files via upload

---
+++ Client/FirstProjectClient/FirstProjectClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FirstProjectClient
+{
+    class Program
+    {
+        static TcpClient client = new TcpClient("127.0.0.1", 5001);
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            StartClient();
+            Thread recieve = new Thread(() => recieveMessages());
+            Thread send = new Thread(() => sendMessages());
+            recieve.Start();
+            send.Start();
+        }
+
+        static void StartClient()
+        {
+            try
+            {
+                stream = client.GetStream();
+                Console.WriteLine("Connected");
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Connection was terminated");
+            }
+        }
+
+        static void recieveMessages()
+        {
+            try
+            {
+                Byte[] bytes = new byte[256];
+                string responseData = string.Empty;
+
+                while (true)
+                {
+                    try
+                    {
+                        Int32 bytes32 = stream.Read(bytes, 0, bytes.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(bytes, 0, bytes32);
+                        Console.WriteLine($"\nThem: {responseData}");
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Lost connection with server...");
+                        StartClient();
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentError {e}");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketError {e}");
+            }
+        }
+
+        static void sendMessages()
+        {
+            try
+            {
+                Byte[] bytes = new byte[256];
+                string responseData = string.Empty;
+                Console.WriteLine("Please enter the message you want to send:");
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("You: ");
+                        responseData = Console.ReadLine();
+                        bytes = System.Text.Encoding.ASCII.GetBytes(responseData);
+                        string ForUser = responseData.Replace("You: ", "");
+                        stream.Write(bytes, 0, bytes.Length);
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Lost connection with server...");
+                        StartClient();
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentError {e}");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketError {e}");
+            }
+        }
+    }
+}
---
+++ Server/FirstProjectServer/FirstProjectServer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FirstProjectServer
+{
+    class Program
+    {
+        static TcpListener server = new TcpListener(IPAddress.Any, 5001);
+        static TcpClient client;
+        static NetworkStream stream;
+
+
+        static void Main(string[] args)
+        {
+            StartServer();
+            Thread recieve = new Thread(() => recieveMessages());
+            Thread send = new Thread(() => sendMessages());
+            recieve.Start();
+            send.Start();
+        }
+
+        static void StartServer()
+        {
+            server.Start();
+            client = server.AcceptTcpClient();
+            stream = client.GetStream();
+            Console.WriteLine("Connected");
+        }
+        
+        static void recieveMessages()
+        {
+            try
+            {
+                Byte[] bytes = new byte[256];
+                string data = null;
+
+                while (true)
+                {
+                    try
+                    {
+                        data = null;
+                        int i;
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i = stream.Read(bytes, 0, bytes.Length));//gets message sent from client
+                        Console.WriteLine($"\nThem: {data}");//displays the clients message
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Lost connection with client...");
+                        StartServer();
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"Socket Exeption: {e}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+        static void sendMessages()
+        {
+            try
+            {
+                string Response = string.Empty;
+                Byte[] bytes = new byte[256];
+                Console.WriteLine("Please enter the message you want to send:");
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("You: ");
+                        Response = Console.ReadLine();
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(Response);
+                        string ForUser = Response.Replace("You: ", "");
+                        // Send back a response to the client
+                        stream.Write(msg, 0, msg.Length);
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Lost connection with client...");
+                        StartServer();
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"Socket Exeption: {e}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
